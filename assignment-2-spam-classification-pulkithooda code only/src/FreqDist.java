e53740300028e1d716d489b12c26ecf5de20a8b4
pu1kith00da <156380500+pu1kith00da@users.noreply.github.com>
2026-01-05 16:28:56 -0800

    Add files via upload

---
+++ assignment-2-spam-classification-pulkithooda code only/src/FreqDist.java
@@ -0,0 +1,66 @@
+//Pulkit Hooda
+/* A freqDist is a wrapper for a Map that will tell us, for a given list of words, how often each word appeared
+in a corpus. You should not need to modify this - you'll only need to use it. */
+
+import java.util.HashMap;
+public class FreqDist
+{
+    HashMap<String, Integer> frequencies;
+    int totalCount;
+
+    public FreqDist()
+    {
+        frequencies = new HashMap<String, Integer>();
+        totalCount = 0;
+    }
+
+    public HashMap<String, Integer> getFrequencies()
+    {
+        return frequencies;
+    }
+
+    public void setFrequencies(HashMap<String, Integer> frequencies)
+    {
+        this.frequencies = frequencies;
+    }
+
+    public int getTotalCount()
+    {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount)
+    {
+        this.totalCount = totalCount;
+    }
+    
+    public void add(String newWord)
+    {
+        int val;
+        if (frequencies.containsKey(newWord))
+        {
+           val = frequencies.get(newWord);
+           frequencies.put(newWord, val+1);
+        }
+        else
+        {
+            frequencies.put(newWord, 1);
+        }
+        totalCount += 1;
+    }
+    
+    /* if a word is not present in our frequency dictionary, assume it has a count of 1. */
+    public int get(String newWord)
+    {
+        if (frequencies.containsKey(newWord))
+        {
+            return frequencies.get(newWord);
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
+
+//this class is completed
